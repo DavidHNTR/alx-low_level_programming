eced3a9ea88d41e54119d6576c3efc281326f011
DavidHNTR <gathagad@gmail.com>
2022-04-07 17:33:22 -0700

    #5 added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Main - print text
+ *
+ * return 0 - success
+ */
+
+int main(void)
+{
+	printf("with proprt grammar, but the outcome is a piece of art,");
+	return (0);
+}
