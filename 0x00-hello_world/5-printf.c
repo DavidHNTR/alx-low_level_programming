c935bdb4e5563c4e8f8e4254a3d08da5c70b1c79
DavidHNTR <gathagad@gmail.com>
2022-04-07 17:36:39 -0700

    fix printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,8 @@
  *
  * return 0 - success
  */
-
 int main(void)
 {
-	printf("with proprt grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
