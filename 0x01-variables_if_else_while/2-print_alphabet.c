8d97f9bdcbe03d69e42ff14e0a1223e7e0e25f08
DavidHNTR <gathagad@gmail.com>
2022-04-11 12:33:23 -0700

    Added task #2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Program start
+ *
+ * Return : Always 0 (Success)
+ */
+int main () {
+
+   char ch;
+
+   for(ch = 'a' ; ch <= 'z' ; ch++)
+   {
+   putchar(ch);
+
+   if(ch == 'z')
+     putchar("\n";
+   }
+
+    return(0);
+}
