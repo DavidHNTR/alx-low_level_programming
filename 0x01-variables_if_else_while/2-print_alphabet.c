99f94d51d1336e48da2fbc44f618fc794ff6da71
DavidHNTR <gathagad@gmail.com>
2022-04-11 12:47:31 -0700

    Added task #2 fix final

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,16 @@
  *
  * Return : Always 0 (Success)
  */
-int main () {
+int main(void)
+{
+int ch;
 
-   int ch;
+for(ch = 0 ; ch < 26 ; ++ch)
+{
+putchar('a' + ch);
+}
 
-   for(ch = 0 ; ch < 26 ; ++ch)
-   {
-   putchar('a' + ch);
-   }
+putchar('\n');
 
-   putchar('\n');
- 
-    return(0);
+return(0);
 }
