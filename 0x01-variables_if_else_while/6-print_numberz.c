de547e26530b1c40cfd919c2a1e0a0f8a7e25fe4
DavidHNTR <gathagad@gmail.com>
2022-04-11 14:19:36 -0700

    Added #6 - try 1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Main proggy
+ *
+ * Return: End of program
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0 ; i < 10 ; i++)
+{
+putchar('0' + i);
+}
+
+putchar('\n');
+
+return (0);
+}
