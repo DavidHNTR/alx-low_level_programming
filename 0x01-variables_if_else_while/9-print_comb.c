a6114fc7667c3fadbab406a2788289c6a29dd3be
DavidHNTR <gathagad@gmail.com>
2022-04-11 16:06:21 -0700

    Added task #7- first take

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Start prog
+ *
+ * Return: End prog
+ * */
+
+int main(void)
+{
+int i;
+for (i = '0' ; i <= '9' ; i++ )
+{
+putchar(i);
+
+if (i != '9')
+{
+putchar(',');
+
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
