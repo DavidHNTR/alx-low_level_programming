efe4c8f50d1ab4e40974c3267b818fc6139932cd
DavidHNTR <gathagad@gmail.com>
2022-04-11 12:57:41 -0700

    First attempt on task #3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Program start
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+int a1, a2;
+
+for (a1=0, a1 < 26, ++a1)
+putchar('a' + a1);
+
+for (a2=0, a2 < 26, ++a2)
+putchar('A' + a2);
+
+putchar('\n');
+
+return (0);
+}
