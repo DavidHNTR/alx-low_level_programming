cef563fc19f3e9f418a352f9895af37b2203f93a
DavidHNTR <gathagad@gmail.com>
2022-04-11 16:59:28 -0700

    advanced #100 - #1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Start here
+ *
+ * Return: End here
+ */
+
+int main(void)
+{
+int i;
+int Num1, Num2;
+for (i = '0' ; i <= '8' ; i++)
+{
+int j;
+for (j = '1' ; j < '9' ; j++)
+{
+Num1 = i + j;
+Num2 = j + i;
+
+if (Num1 == Num2 || Num2<Num1)
+continue;
+
+if(i > '0' || j > '0')
+{
+putchar(',');
+putchar(' ');
+}
+
+putchar(i);
+putchar(j);
+
+}
+
+}
+
+putchar('\n');
+
+return (0);
+}
