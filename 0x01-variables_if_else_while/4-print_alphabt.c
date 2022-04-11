103a457ba9e9354142a3cc5d3ef93b513762365c
DavidHNTR <gathagad@gmail.com>
2022-04-11 13:07:01 -0700

    Added task #4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c;
+
+for (c = 0; c < 26 ; ++c)
+{
+if('a' + c == 'e' || 'a' + c == 'q')
+continue;
+
+putchar('a' + c);
+}
+
+putchar('\n');
+
+return (0);
+}
