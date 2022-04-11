4bf8ccdb81eb6041be052b9fb90a13b5eb0ebb34
DavidHNTR <gathagad@gmail.com>
2022-04-11 12:59:05 -0700

    First attempt on task #3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,10 @@ int main(void)
 {
 int a1, a2;
 
-for (a1=0, a1 < 26, ++a1)
+for (a1=0; a1 < 26; ++a1)
 putchar('a' + a1);
 
-for (a2=0, a2 < 26, ++a2)
+for (a2=0; a2 < 26; ++a2)
 putchar('A' + a2);
 
 putchar('\n');
