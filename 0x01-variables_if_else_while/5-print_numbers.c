adfb06090befcf7fc62a92eca366d7335bd65446
DavidHNTR <gathagad@gmail.com>
2022-04-11 13:46:59 -0700

    first try on task #5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program start
+ *
+ * Return: End SUCCESS
+ */
+
+int main(void)
+{
+for (int a = 0; a < 10; a++)
+{
+printf(a);
+}
+
+print("\n");
+
+return (0);
+}
