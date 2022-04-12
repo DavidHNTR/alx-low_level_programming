00c74055c2457dfe23dd5e780329a13832b86321
DavidHNTR <gathagad@gmail.com>
2022-04-12 13:33:26 -0700

    Added task #1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Start
+ *
+ * Return: End
+ */
+
+void print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
+
+int main(void)
+{
+print_alphabet();
+
+return (0);
+}
