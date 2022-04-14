140a9da386df4596594243e1a578122ea0704e10
DavidHNTR <gathagad@gmail.com>
2022-04-13 17:30:10 -0700

    Added task #2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Start
+ *
+ * Return: End
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+int j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+}
+
+_putchar('\n');
+}
