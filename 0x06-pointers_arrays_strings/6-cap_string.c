ad7cdc5282e426ec53ccc4b0c94ec13844ee1d61
DavidHNTR <87418871+DavidHNTR@users.noreply.github.com>
2022-04-26 05:33:53 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,27 +10,27 @@ char *cap_string(char *s)
 
 	while (s[index])
 	{
-	  while (!(s[index] >= 'a' && s[index] <= 'z'))
+		while (!(s[index] >= 'a' && s[index] <= 'z'))
+			index++;
+
+		if (s[index - 1] == ' ' ||
+		    s[index - 1] == '\t' ||
+		    s[index - 1] == '\n' ||
+		    s[index - 1] == ',' ||
+		    s[index - 1] == ';' ||
+		    s[index - 1] == '.' ||
+		    s[index - 1] == '!' ||
+		    s[index - 1] == '?' ||
+		    s[index - 1] == '"' ||
+		    s[index - 1] == '(' ||
+		    s[index - 1] == ')' ||
+		    s[index - 1] == '{' ||
+		    s[index - 1] == '}' ||
+		    index == 0)
+			s[index] -= 32;
+
 		index++;
-
-	     if (s[index - 1] == ' ' ||
-		s[index - 1] == '\t' ||
-		s[index - 1] == '\n' ||
-		s[index - 1] == ',' ||
-		s[index - 1] == ';' ||
-		s[index - 1] == '.' ||
-		s[index - 1] == '!' ||
-		s[index - 1] == '?' ||
-		s[index - 1] == '"' ||
-		s[index - 1] == '(' ||
-		s[index - 1] == ')' ||
-		s[index - 1] == '{' ||
-		s[index - 1] == '}' ||
-		index == 0)
-		s[index] -= 32;
-
-	index++;
 	}
 
-return (s);
+	return (s);
 }
